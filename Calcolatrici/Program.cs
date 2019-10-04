4b19771a08e0c5b8fd94d5d296c1975547ae900f
nunzio22 <nunzio22598@gmail.com>
2019-10-04 12:01:11 +0200

    Frist commit of sourcecode

---
+++ Calcolatrici/Program.cs
@@ -0,0 +1,60 @@
+﻿using MyOperazioni;
+using MyInserimento;
+using System;
+
+namespace Calcolatrici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Op;
+            int n1;
+            int n2;
+            int ris=0;
+            string risp="";
+            Console.WriteLine("Benvenuti in Umana Caluletor scegliere l'operazione che si vuole fare");
+            do
+            {
+                Op = Inserimento.InsertOp();
+                Console.WriteLine("L'operazione selezionata è la N:" + Op);
+                Console.WriteLine(Op);
+                if (risp == "Si" || risp == "si")
+                {
+                    n1 = ris;
+                }     
+                else
+                {
+                    n1 = Inserimento.insertN();
+                }
+                n2 = Inserimento.insertN();
+                if (Op == 5)
+                {
+                    Operazioni.resto(n1, n2);
+                }
+                else
+                {
+                    ris = Operazioni.op(n1, n2, Op);
+                    Console.WriteLine("Il risultato è:" + ris);
+                    Console.ReadLine();
+                }
+                Console.WriteLine("Vuoi vare altre operazioni?");
+                risp = Console.ReadLine();
+
+                if (risp == "Si" || risp == "si")
+                    ;
+                else
+                    break;
+                Console.WriteLine("Vuoi tenere in memoria il risultato?");
+                risp = Console.ReadLine();
+
+
+            } while (true);
+
+
+
+
+
+        }
+    }
+}
